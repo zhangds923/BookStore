1cf5b995aa6de7295c4de65669d135a693a8bcab
zhangds <i@iilii.cc>
2021-01-07 15:09:30 +0800

    1.导入了MySQL数据库连接驱动和Druid数据库连接池jar包 2.编写数据库池的连接配置文件jdbc.properties 3.编写工具类JdbcUtils用来获取连接和关闭连接

---
+++ src/com/zds/utils/JdbcUtils.java
@@ -0,0 +1,66 @@
+package com.zds.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.alibaba.druid.pool.DruidPooledConnection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class JdbcUtils {
+    private static DruidDataSource druidDataSource = null;
+    //初始化数据库连接池
+    static {
+        try{
+            Properties properties = new Properties();
+            //读取配置文件
+            InputStream is = JdbcUtils.class.getClassLoader().getResourceAsStream("jdbc.properties");//方法一
+            FileInputStream fis = new FileInputStream(new File("src/jdbc.properties"));//方法而
+            //从流中加载数据
+            properties.load(is);
+            //创建数据库连接池
+            druidDataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 用来获取数据库连接池中的连接
+     * @return 如果返回null，说明获取连接失败，有值就是获取成功
+     */
+    public static Connection getConnection(){
+        DruidPooledConnection connection = null;
+        try{
+            //获取连接
+            connection = druidDataSource.getConnection();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        //返回连接
+        return connection;
+    }
+
+    /**
+     * 用来关闭数据库连接池中的连接
+     * @param connection
+     */
+    public static void close(Connection connection){
+        if (connection != null){
+            try{
+                connection.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+}
